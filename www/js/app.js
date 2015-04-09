d126091d09f207bbc9b6ff8734141460c42c0302
VladimirAus <v_roudakov@yahoo.com>
2015-04-09 22:01:46 +1000

    App: creating data structure, adding content types and taxonomies

--- www/js/app.js
+++ www/js/app.js
@@ -1,28 +1,55 @@
-angular.module('todoApp', [])
-  .controller('TodoListController', function() {
-    var todoList = this;
-    todoList.todos = [
-      {text:'learn angular', done:true},
-      {text:'build an angular app', done:false}];
+angular.module('drupalLacesApp', [])
+  .controller('DrupalStructController', function () {
+    var drupalStruct = this;
+    drupalStruct.contentTypes = [
+      {typeTitle: 'Article', machineName: 'article', selected: false, fields: []},
+      {typeTitle: 'Page', machineName: 'page', selected: false, fields: []}
+    ];
+    drupalStruct.taxonomies = [
+      {taxName: 'Tags', machineName: 'tags', selected: false, fields: []}
+    ];
  
-    todoList.addTodo = function() {
-      todoList.todos.push({text:todoList.todoText, done:false});
-      todoList.todoText = '';
+    drupalStruct.addTodo = function (type) {
+      if (type == 'ctype') {
+        drupalStruct.contentTypes.push({
+          typeTitle: drupalStruct.typeName, 
+          machineName: drupalStruct.convrtToMachineName(drupalStruct.typeName), 
+          selected: false, 
+          fields: []
+        });
+      }
+      else if (type == 'taxonomy') {
+        drupalStruct.taxonomies.push({
+          taxName: drupalStruct.typeName, 
+          machineName: drupalStruct.convertToMachineName(drupalStruct.typeName), 
+          selected: false, 
+          fields: []
+        });
+      }
+      drupalStruct.typeName = '';
     };
  
-    todoList.remaining = function() {
+    drupalStruct.convertToMachineName = function (title) {
+      return title.replace(' ', '_').replace('  ', ' ').toLower();
+    };
+    drupalStruct.remaining = function () {
       var count = 0;
-      angular.forEach(todoList.todos, function(todo) {
-        count += todo.done ? 0 : 1;
+      angular.forEach(drupalStruct.contentTypes, function(contentType) {
+        count += contentType.selected ? 0 : 1;
       });
       return count;
     };
  
-    todoList.archive = function() {
-      var oldTodos = todoList.todos;
-      todoList.todos = [];
-      angular.forEach(oldTodos, function(todo) {
-        if (!todo.done) todoList.todos.push(todo);
+    drupalStruct.archive = function () {
+      var oldTypes = drupalStruct.contentTypes;
+      drupalStruct.contentTypes = [];
+      angular.forEach(oldTypes, function(contentType) {
+        if (!contentType.selected) drupalStruct.contentTypes.push(contentType);
+      });
+      var oldTaxonomies = drupalStruct.taxonomies;
+      drupalStruct.contentTypes = [];
+      angular.forEach(oldTaxonomies, function(taxonomy) {
+        if (!taxonomy.selected) drupalStruct.contentTypes.push(taxonomy);
       });
     };
   });
